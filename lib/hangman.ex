9e1a36a1aec3bbe4c7eb18ac9e3d8d798203e0f0
Ian Johnson <ianjjohnson@icloud.com>
2016-10-17 18:29:18 -0500

    Add supervision description

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,6 +1,30 @@
 defmodule Hangman do
   use Application
 
+  #   For my supervision strategy, I decided to use a two-tiered supervision tree,
+  # where the root supervisor has two children: the dictionary, and another supervisor.
+  # This root supervisor uses the one_for_all restart strategy, so that if the dictionary
+  # crashes, all of the games will be stopped (the games are supervised by the second-layer
+  # supervisor). The second-layer supervisor (which supervises the hangman game servers),
+  # in turn, uses a one-for-one strategy, so that whenever a game crashes, that game
+  # and that game alone is re-started. For the game server workers, the transient restart
+  # strategy is used, because the game should restart, unless it exited normally. For the
+  # dictionary worker, the permanent restart strategy is used, because the dictionary
+  # must be up at all times, at it is a dependency for the game server workers.
+
+  # Here is an attempt at a drawing of the supervision tree:
+
+            #####################################
+            #   Root Supervisor (this module)   #
+            #         /            \            #
+            #        /              \           #
+            #       /                \          #
+            # Dictionary        Game Supervisor #
+            #                         |         #
+            #                         |         #
+            #                     GameServer    #
+            #####################################
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -13,7 +37,7 @@ defmodule Hangman do
 
       worker(Hangman.Dictionary, [],
              id: :dictionary,
-             restart: :temporary
+             restart: :permanent
      ),
 
       supervisor(GameServerSupervisor, [])
