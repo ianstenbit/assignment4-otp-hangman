ce4da5db5f19d84f85237a8d180ccd36f2897296
Omar Roa <omarfroa@gmail.com>
2016-10-25 22:53:45 -0500

    uncommenting stuff

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -11,13 +11,12 @@ defmodule Hangman do
 
     # Uncomment and complete this:
 
-    # import Supervisor.Spec, warn: false
-    # 
-    # children = [
-    # ]
-    # 
-    # opts = [strategy: :you_choose_a_strategy, name: Hangman.Supervisor]
-    # Supervisor.start_link(children, opts)
+    import Supervisor.Spec, warn: false
+
+    children = [
+    ]
+
+    opts = [strategy: :you_choose_a_strategy, name: Hangman.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
-
