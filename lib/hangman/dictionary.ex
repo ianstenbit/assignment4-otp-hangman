82b7a9724ef8bc774b574534790b9b0f911266a6
Omar Roa <omarfroa@gmail.com>
2016-10-30 23:17:03 -0500

    edits to dictionary.ex

--- lib/hangman/dictionary.ex
+++ lib/hangman/dictionary.ex
@@ -2,8 +2,6 @@ defmodule Hangman.Dictionary do
   use GenServer
   @me :dictionary
 
-
-
   @moduledoc """
   We act as an interface to a wordlist (whose name is hardwired in the
   module attribute `@word_list_file_name`). The list is formatted as
@@ -19,8 +17,8 @@ defmodule Hangman.Dictionary do
   @doc """
   Start GenServer and start with the word list.
   """
-  def start_link(word_list) do
-    GenServer.start_link(__MODULE__, word_list, :name @me  )
+  def start_link(words) do
+    GenServer.start_link(__MODULE__, words, name: @me  )
   end
 
   @doc """
@@ -46,29 +44,32 @@ defmodule Hangman.Dictionary do
   ###########################
 
   # Page 225 of Elixir Text
-  def init (word_list) do: { :ok, word_list }
+  def init(args), do: { :ok, args }
 
   # Page 225-226 of Elixir Text & Lecture 16
   def handle_call({ :random_word }, _from, state) do
     {
       :reply,
-     word_list
-     |> random_word,
-     state
-    }
-  end
-
-  # Page 225-226 of Elixir Text & Lecture 16
-  def handle_call({ :words_of_length, len }, _from, state) do
-    {
-      :reply,
       word_list
-      |> words_of_length(len),
+      |> Enum.random
+      |> String.trim,
       state
     }
   end
 
-
+  @doc """
+  Return a list of all the words in our word list of a given length.
+  Whitespace and newlines will have been removed.
+  """
+  def handle_call({ :words_of_length, len }, _from, state) do
+    {
+      :reply,
+      word_list
+      |> Stream.map(&String.trim/1)
+      |> Enum.filter(&(String.length(&1) == len)),
+      state
+    }
+  end
 
   ###########################
   # End of public interface #
@@ -78,31 +79,6 @@ defmodule Hangman.Dictionary do
     @word_list_file_name
     |> File.open!
     |> IO.stream(:line)
-  end
-
-  # Keeping below so I can call them in handle call
-
-  @doc """
-  Return a random word from our word list. Whitespace and newlines
-  will have been removed.
-  """
-
-  @spec random_word() :: binary
-  defp random_word do
-    word_list
-    |> Enum.random
-    |> String.trim
-  end
-
-  @doc """
-  Return a list of all the words in our word list of a given length.
-  Whitespace and newlines will have been removed.
-  """
-  @spec words_of_length(integer)  :: [ binary ]
-  defp words_of_length(len) do
-    word_list
-    |> Stream.map(&String.trim/1)
-    |> Enum.filter(&(String.length(&1) == len))
   end
 
 end
